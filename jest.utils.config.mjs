0eb3edcb7acbe08421b6b5f46d2748927afbe365
Arnms <incheol323@gmail.com>
2025-07-12 19:31:15 +0900

    테스트 환경 설정 및 Jest 구성 수정
        - Jest moduleNameMapper 오타 수정 (moduleNameMapping → moduleNameMapper)
        - 테스트용 환경변수 스키마에 silent 로그 레벨 추가
        - 테스트 환경에서 process.exit 대신 에러 throw로 변경
        - 테스트 데이터베이스 자동 마이그레이션 설정 추가
        - 유틸리티 테스트와 통합 테스트 분리 구성

---
+++ jest.utils.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests/utils'],
+  testMatch: ['**/utils/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true
+    }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
